fd631bd3085177d8b24d3ce319356fc9b970eea5
JAEDEOKJO <lubiksss@gmail.com>
2021-07-19 01:39:28 +0900

    update_training

--- 3_NEURALNETWORK/multidim_array.py
+++ 3_NEURALNETWORK/multidim_array.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys, os
-sys.path.append(os.getcwd())
-from lib.activation_function import *
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/lib')
+from activation_function import *
 
 X = np.array([1.0,0.5])
 W1 = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
